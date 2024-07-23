77935eabb697f329e990831ffa458049283967bd
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-07-23 08:24:55 -0300

    Fix remove cart products

--- app/src/main/java/com/example/pasionariastore/model/CartUIState.kt
+++ app/src/main/java/com/example/pasionariastore/model/CartUIState.kt
@@ -1,10 +1,12 @@
 package com.example.pasionariastore.model
 
+import androidx.compose.runtime.mutableStateListOf
+
 data class CartUIState(
     val canSearchProducts: Boolean = false,
     val currentSearch: String = "",
     val showModalProductSearch: Boolean = false,
-    val productCartList: MutableList<ProductCart> = mutableListOf(),
+    val productCartList: MutableList<ProductCart> = mutableStateListOf(),
     val currentProductSearcheds: List<Product> = mutableListOf(),
     val currentProductCart: ProductCart? = null
 ) {
