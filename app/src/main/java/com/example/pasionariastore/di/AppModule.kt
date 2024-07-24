ae1afa3c64545454a27eb8360f360c1b204e5fac
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-07-24 08:48:52 -0300

    fix module

--- app/src/main/java/com/example/pasionariastore/di/AppModule.kt
+++ app/src/main/java/com/example/pasionariastore/di/AppModule.kt
@@ -8,10 +8,12 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
 @Module
-@InstallIn(Singleton::class)
+@InstallIn(SingletonComponent::class)
+
 object AppModule {
     @Singleton
     @Provides
