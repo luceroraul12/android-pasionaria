e0874763e1b01c2fbae890a50b77ee731f43c015
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-07-26 22:04:03 -0300

    fix implementations

--- app/src/androidTest/java/com/example/pasionariastore/repository/CartRepositoryFake.kt
+++ app/src/androidTest/java/com/example/pasionariastore/repository/CartRepositoryFake.kt
@@ -1,19 +1,13 @@
 package com.example.pasionariastore.repository
 
+import com.example.pasionariastore.data.Datasource
 import com.example.pasionariastore.model.ProductCart
 import com.example.pasionariastore.model.ProductCartWithData
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 class CartRepositoryFake : CartRepository {
-    override fun getProducts(): Flow<List<ProductCartWithData>> {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun insertProductCart(productCart: ProductCart) {
-        TODO("Not yet implemented")
-    }
-
-    override suspend fun deleteProductCart(productCart: ProductCart) {
-        TODO("Not yet implemented")
-    }
+    override fun getProducts(): Flow<List<ProductCartWithData>> = flow { Datasource.cartProducts }
+    override suspend fun insertProductCart(productCart: ProductCart) {}
+    override suspend fun deleteProductCart(productCart: ProductCart) {}
 }
