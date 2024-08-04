741c343161f642c8aced5adaa09da6a5630bfb50
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-08-04 12:13:05 -0300

    Update BackendModels

--- app/src/main/java/com/example/pasionariastore/model/BackendProduct.kt
+++ app/src/main/java/com/example/pasionariastore/model/BackendProduct.kt
@@ -1,5 +1,19 @@
 package com.example.pasionariastore.model
 
 data class BackendProduct(
-    val id: Int = 0
+    val id: Long,
+    val stock: Boolean,
+    val onlyUnit: Boolean,
+    val name: String,
+    val description: String? = null,
+    val price: Long,
+    val category: BackendLookup,
+    val unitType: BackendLookup
+)
+
+data class BackendLookup(
+    val id: Long,
+    val code: String,
+    val description: String,
+    val value: String? = null
 )
