0b32c23c1f02e945b1fd42ae7d3f516c1ac2a308
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-08-04 17:32:20 -0300

    Add generic TopBar

--- app/src/main/java/com/example/pasionariastore/components/Components.kt
+++ app/src/main/java/com/example/pasionariastore/components/Components.kt
@@ -1,15 +1,75 @@
 package com.example.pasionariastore.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.pasionariastore.R
+import com.example.pasionariastore.ui.theme.PasionariaStoreTheme
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
-fun MainTopBar() {
-    TopAppBar(title = { Text(text = "Texto") })
+private fun MainTopBarFinalizeOnPreview() {
+    PasionariaStoreTheme {
+        MainTopBar(title = "Titulo ejemplo", onBackClicked = {}, showBackIcon = true, actions = {
+            FinalizeButton(onFinalize = {}, canFinalize = true)
+        })
+    }
+}
+
+@Preview
+@Composable
+private fun MainTopBarFinalizeOffPreview() {
+    PasionariaStoreTheme {
+        MainTopBar(title = "Titulo ejemplo", onBackClicked = {}, showBackIcon = true, actions = {
+            FinalizeButton(onFinalize = {})
+        })
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainTopBar(
+    title: String,
+    onBackClicked: () -> Unit,
+    showBackIcon: Boolean = false,
+    actions: @Composable() (RowScope.() -> Unit)
+) {
+    TopAppBar(
+        title = { Text(text = "Texto", fontWeight = FontWeight.Bold, color = Color.White) },
+        colors = TopAppBarDefaults.mediumTopAppBarColors(containerColor = MaterialTheme.colorScheme.primary),
+        navigationIcon = {
+            if (showBackIcon) {
+                Icon(imageVector = Icons.Default.ArrowBack,
+                    contentDescription = "",
+                    tint = Color.White,
+                    modifier = Modifier.clickable { onBackClicked() })
+            }
+        },
+        actions = actions
+    )
+}
+
+@Composable
+fun FinalizeButton(onFinalize: () -> Unit, canFinalize: Boolean = false) {
+    Button(
+        onClick = onFinalize,
+        colors = ButtonDefaults.buttonColors(containerColor = colorResource(id = if (canFinalize) R.color.finalized_active else R.color.finalized_inactive)),
+    ) {
+        Text(text = "Finalizar")
+    }
 }
