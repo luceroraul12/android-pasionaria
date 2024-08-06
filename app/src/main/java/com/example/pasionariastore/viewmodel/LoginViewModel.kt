6b2ca1cbe8c0b074615a48853f9fc4ce6e74919b
Raul Lucero <106817372+luceroraul12@users.noreply.github.com>
2024-08-06 07:24:11 -0300

    Fix response backend error

--- app/src/main/java/com/example/pasionariastore/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/example/pasionariastore/viewmodel/LoginViewModel.kt
@@ -4,14 +4,17 @@ import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.pasionariastore.data.api.ApiBackend
+import com.example.pasionariastore.model.BackendErrorResponse
 import com.example.pasionariastore.model.BackendLogin
 import com.example.pasionariastore.model.state.LoginUIState
+import com.google.gson.Gson
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
+import okhttp3.ResponseBody
 import javax.inject.Inject
 
 @HiltViewModel
@@ -30,15 +33,29 @@ class LoginViewModel @Inject constructor(
     }
 
     fun login() {
-//        viewModelScope.launch {
-//            withContext(Dispatchers.IO) {
-//                state.value.run {
-//                    val response =
-//                        apiBackend.login(BackendLogin(username = username, password = password))
-//                    Log.i("login", response.toString())
-//                }
-//            }
-//        }
+        viewModelScope.launch {
+            withContext(Dispatchers.IO) {
+                state.value.run {
+                    val response =
+                        apiBackend.login(BackendLogin(username = username, password = password))
+                    var data: ResponseBody? = response.errorBody()
+                    Log.i("login", "prueba object ${response.toString()}")
+                    Log.i("login", "body success ${response.body()}")
+
+                    val errorBodyString = response.errorBody()?.string() ?: ""
+
+                    // Crear una instancia de Gson
+                    val gson = Gson()
+
+                    // Convertir la cadena JSON a un objeto de tu clase de error
+                    val errorResponse =
+                        gson.fromJson(errorBodyString, BackendErrorResponse::class.java)
+
+                    // Ahora puedes acceder a los campos de errorResponse
+                    Log.i("login", errorResponse.message)
+                }
+            }
+        }
     }
 
 }
